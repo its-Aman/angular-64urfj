95f2dba23d6d4b189e81877d6d0b4c36e28923fa
its-Aman <kumaraman7056@hotmail.com>
2022-01-27 17:08:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-64urfj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-64urfj)
---
+++ src/app/root-another/root-another.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root-another',
+  templateUrl: './root-another.component.html',
+  styleUrls: ['./root-another.component.css']
+})
+export class RootAnotherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
